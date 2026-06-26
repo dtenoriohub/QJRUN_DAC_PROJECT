c7a87a1b3235e6fa92ed847910acea19bcb4184f
Sabrina Raiane L. Barbosa <sabrinaraiane2003@gmail.com>
2026-06-26 14:47:38 -0300

    refactor(repository): mover findByEmail para UsuarioRepository após introdução da herança

--- src/main/java/com/qjrun/qjrun/repository/AdministradorRepository.java
+++ src/main/java/com/qjrun/qjrun/repository/AdministradorRepository.java
@@ -5,11 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface AdministradorRepository extends JpaRepository<Administrador,Long> {
-    Optional<Administrador> findByEmail(String email);
 
     List<Administrador> findAllByAtivoTrue();
 }
