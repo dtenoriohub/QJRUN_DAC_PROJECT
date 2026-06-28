cef40fc1fc81e9899c0aa633be11b05e53ac464e
danilo tenorio <danilomont12345@gmail.com>
2026-06-27 23:21:01 -0300

    chore: atualizacao pom.xml

--- src/main/java/com/qjrun/qjrun/repository/UsuarioRepository.java
+++ src/main/java/com/qjrun/qjrun/repository/UsuarioRepository.java
@@ -10,4 +10,7 @@ import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
 }
